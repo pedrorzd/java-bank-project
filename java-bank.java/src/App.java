b351c8f3b2e99ed06125a9abd942c0817e2f8e26
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2024-05-28 22:17:27 -0300

    git commit -m “Finalização do menu inicial”

--- java-bank.java/src/App.java
+++ java-bank.java/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
 
         // espaço para colocar variáveis//
-        int option = 0;
+        int option = 0, i = 0;
         double currentBalance, savingsBalance;
         String clientName, accountCurrentNumber, savingsAccountNumber;
 
@@ -15,11 +15,12 @@ public class App {
         Scanner inputCurrentBalance = new Scanner(System.in);
         Scanner inputSavingsBalance = new Scanner(System.in);
         Scanner inputOption = new Scanner(System.in);
+        Scanner inputMenuSelector = new Scanner(System.in);
 
         System.out.println(
                 "Olá bem vindo ao nosso sistema bancário, para dar início ao serviço, insira os dados necessários para fazer a verificação de sua conta bancária.");
         System.out.println();// para pular uma linha apos a impressao da mensagem de incio
-        
+
         System.out.print("Digite o nome do cliente: ");
         clientName = inputClientName.nextLine();
 
@@ -38,34 +39,112 @@ public class App {
         System.out.println();
 
         do {
+            System.out.println("Qual operação deseja realizar?");
             System.out.println();
             System.out.println("1 - Creditar");
             System.out.println("2 - Debitar");
             System.out.println("3 - Transferir");
-            System.out.println("4 - Saldo");
+            System.out.println("4 - Consultar Saldo");
             System.out.println("5 - Sair");
             System.out.println();
             option = inputOption.nextInt();
 
-            if (option == 1) {
-                System.out.println("Você escolheu a opção de CREDITAR");
-                // colocar codigo
-            } else if (option == 2) {
-                System.out.println("Você escolheu a opção de DEBITAR");
-                // colocar codigo
-            } else if (option == 3) {
-                System.out.println("Você escolheu a opção de TRANSFERIR");
-                // colocar codigo
-            } else if (option == 4) {
-                System.out.println("Você escolheu a opção de CONSULTAR SALDO");
-                // colocar codigo
-            } else if (option == 5) {
-                System.out.println("Você escolheu a opção de SAIR");
-                // operação encerrada pelo cliente
-            } else {
-                System.out.println("Operação Inválida!");
+            switch (option) {
+                case 1:
+                    System.out.println("Você escolheu a opção de CREDITAR");
+                    do {
+                        System.out.println("Em qual conta será feito o CRÉDITO?");
+                        System.out.println("1 - Conta Corrente");
+                        System.out.println("2 - Conta Poupança");
+                        System.out.println("5 - SAIR");
+                        System.out.println();
+                        i = inputMenuSelector.nextInt();
+
+                        if (i == 1) {
+                            // creditar conta corrente
+                        } else if (i == 2) {
+                            // creditar conta corrente
+                        } else {
+                            System.out.println("Operação Inválida");
+                            System.out.println();
+                        }
+                    } while (i != 5);
+                    break;
+
+                case 2:
+                    System.out.println("Você escolheu a opção de DEBITAR");
+                    do {
+                        System.out.println("Em qual conta será feito o DÉBITO?");
+                        System.out.println("1 - Conta Corrente");
+                        System.out.println("2 - Conta Poupança");
+                        System.out.println("5 - SAIR");
+                        System.out.println();
+                        i = inputMenuSelector.nextInt();
+
+                        if (i == 1) {
+                            // DEBITAR conta corrente
+                        } else if (i == 2) {
+                            // DEBITAR conta corrente
+                        } else {
+                            System.out.println("Operação Inválida");
+                            System.out.println();
+                        }
+                    } while (i != 5);
+                    break;
+
+                case 3:
+                    System.out.println("Você escolheu a opção de TRANSFERIR");
+                    do {
+                        System.out.println("A valor será retirado de qual conta?");
+                        System.out.println("1 - Conta Corrente");
+                        System.out.println("2 - Conta Poupança");
+                        System.out.println("5 - SAIR");
+                        System.out.println();
+                        i = inputMenuSelector.nextInt();
+
+                        if (i == 1) {
+                            // TRANSFERIR conta corrente
+                        } else if (i == 2) {
+                            // TRANSFERIR conta corrente
+                        } else {
+                            System.out.println("Operação Inválida");
+                            System.out.println();
+                        }
+                    } while (i != 5);
+                    break;
+
+                case 4:
+                    System.out.println("Você escolheu a opção de CONSULTAR SALDO");
+                    do {
+                        System.out.println("Em qual conta será feita a CONSULTA?");
+                        System.out.println("1 - Conta Corrente");
+                        System.out.println("2 - Conta Poupança");
+                        System.out.println("5 - SAIR");
+                        System.out.println();
+                        i = inputMenuSelector.nextInt();
+
+                        if (i == 1) {
+                            // CONSULTAR conta corrente
+                        } else if (i == 2) {
+                            // CONSULTAR conta corrente
+                        } else {
+                            System.out.println("Operação Inválida");
+                            System.out.println();
+                        }
+                    } while (i != 5);
+                    break;
+
+                case 5:
+                    // operação encerrada pelo cliente
+                    System.out.println("Você escolheu a opção de SAIR");
+                    break;
+
+                default:
+                    System.out.println("Operação Inválida!");
+                    System.out.println();
+                    break;
             }
         } while (option != 5);
-        
+
     }
 }
